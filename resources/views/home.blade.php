b2b97bc7417241aa0ac33d8ba28cf4439b85e301
Marat Petrosov <marat.petrosov@gmail.com>
2019-05-27 16:53:01 +0200

    beatified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,15 @@
 <div class="circle two"></div>
 <div class="circle three"></div>
 @section('head')
-<div class="row own-color">
+<div class="row own-color jumbotron">
   <div class="col-2">
-    <a class="navbar-brand" href="#"><img src="{{URL::asset('/img/Logo-Motionn2 klein.png')}}" class="rounded" alt="profile Pic" height="100" width="200"></a>
+    <a class="" href="#"><img src="{{URL::asset('/img/Logo-Motionn2 klein.png')}}" class="rounded" alt="profile Pic" height="100" width="200"></a>
   </div>
-  <div class="col-3 d-flex align-items-end">
+  <div class="col-7 d-flex align-items-end">
       <h3 class="motion">Motionn Garagebedrijf B.V.</h3>
   </div>
-  <div class="col-4">
-      {{-- <h4>Calandstraat 89, Schideam</h4>
-      <h5>010-7518506</h5>
-      <h6>Maandag t/m zaterdag</h6>
-      <h6>van 9:00 tot 17:45 uur</h6> --}}
-  </div>
   <div class="col-3 contact">
-        <h4>Calandstraat 89, Schideam</h4>
+        <h4>Calandstraat 89, Schiedeam</h4>
         <h5>010-7518506</h5>
         <h6>Maandag t/m zaterdag</h6>
         <h6>van 9:00 tot 17:45 uur</h6>
@@ -41,20 +35,20 @@
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
         </ol>
         <div class="row justify-content-center">
-            <div class="col-4">
+            <div class="col-6">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="{{URL::asset('/img/motor.jpg')}}" class="rounded" alt="profile Pic" height="300" width="500">
                         <div class="carousel-caption own-caption">
-                            <h5 class="animated fadeInUp" style="animation-delay: 1s">First slide label</h5>
-                            <h6 class="animated fadeInUp" style="animation-delay: 2s">Nulla vitae elit libero, a pharetra augue mollis interdum.</h6>
+                            <h5>First slide label</h5>
+                            <h6>Nulla vitae elit libero, a pharetra augue mollis interdum.</h6>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src="{{URL::asset('/img/schadeherstel.jpg')}}" class="rounded" alt="profile Pic" height="300" width="500">
                         <div class="carousel-caption own-caption">
-                            <h5 class="animated fadeInLeft" style="animation-delay: 1s">Second slide label</h5>
-                            <h6 class="animated fadeInRight" style="animation-delay: 2s"> Nulla vitae elit libero, a pharetra augue mollis interdum.</h6>
+                            <h5>Second slide label</h5>
+                            <h6> Nulla vitae elit libero, a pharetra augue mollis interdum.</h6>
                         </div>
                     </div>
                     <div class="carousel-item">
@@ -83,20 +77,20 @@
 @section('gridboven')
     
     <div class="row text-center">
-        <div class="col-md-3 shadow rounded client inner p-0">
-            <img src="{{URL::asset('/img/motor.jpg')}}" class="p-0" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client inner p-0">
+            <img src="{{URL::asset('/img/motor.jpg')}}" class="p-0" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption client-align"><h2 class="my-auto">Inspectie en Reparatie Motor</h2></div>
         </div>
-        <div class="col shadow rounded client ml-2 inner p-0">
-            <img src="{{URL::asset('/img/Turbo.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/Turbo.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2>Turbo Revisie en Reparatie</h2></div>
         </div>
-        <div class="col shadow rounded client ml-2 inner p-0">
-            <img src="{{URL::asset('/img/computer diagnostic.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/computer diagnostic.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2> Uitlezen van Foutcodes</h2></div>
         </div>
-        <div class="col-md-3 shadow rounded client ml-2 inner p-0">
-            <img src="{{URL::asset('/img/schadeherstel.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/schadeherstel.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2>Schadeherstel en Spuitwerk</h2></div>
         </div>
     </div>
@@ -105,58 +99,48 @@
 
 @section('content')
      <div class="row text-center">
-            <div class="col-3 shadow rounded client mr-2 inner p-0">
-                <img src="{{URL::asset('/img/ophanging.png')}}" class="rounded" alt="profile Pic" height="400" width="400">
-                <div class="carousel-caption"><h2 class="client-align">Inspectie en Reparatie Ophanging</h2></div>
-            </div>
-            <div class="col ml-1 mr-1 shadow rounded client inner p-0 ">
-                <img src="{{URL::asset('/img/bigpic.jpg')}}" alt="profile Pic" height="400" width="720">
-            </div>
-            <div class="col-3 shadow rounded client ml-2 inner p-0">
-                <img src="{{URL::asset('/img/airco.jpg')}}" class="rounded" alt="profile Pic" height="400" width="380">
-                <div class="carousel-caption"><h2>Airco service</h2></div>
-            </div>
+        <div class="col-3 shadow rounded client mr-2 inner p-0">
+            <img src="{{URL::asset('/img/ophanging.png')}}" class="rounded" alt="profile Pic" height="400" width="300">
+            <div class="carousel-caption"><h2 class="client-align">Inspectie en Reparatie Ophanging</h2></div>
+        </div>
+        <div class="col ml-1 mr-1 shadow rounded client inner p-0 ">
+            <img src="{{URL::asset('/img/bigpic.jpg')}}" alt="profile Pic" height="400" width="720">
+        </div>
+        <div class="col-3 shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/airco.jpg')}}" class="rounded" alt="profile Pic" height="400" width="300">
+            <div class="carousel-caption"><h2>Airco service</h2></div>
+        </div>
     </div>
         <!--/row-->
   
 @endsection
 @section('gridonder')
     <div class="row text-center">
-        <div class="col-md-3 shadow rounded client inner p-0">
-            <img src="{{URL::asset('/img/treshina-lobovogo-stekla.jpg')}}" alt="profile Pic" height="200" width="367">
-            <div class="carousel-caption mx-auto"><h2>Ruitreparatie</h2></div>
+        <div class="col-lg shadow rounded client inner p-0">
+            <img src="{{URL::asset('/img/treshina-lobovogo-stekla.jpg')}}" alt="profile Pic" height="200" width="300">
+            <div class="carousel-caption"><h2>Ruitreparatie</h2></div>
         </div>
                
-        <div class="col shadow rounded client ml-2 inner p-0">
-            <img src="{{URL::asset('/img/onderhoud.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/onderhoud.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2>Onderhoud</h2></div>
         </div>
-        <div class="col shadow rounded client ml-2 inner p-0">
-                <img src="{{URL::asset('/img/Injectiesysteem Reinigen.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client ml-2 inner p-0">
+                <img src="{{URL::asset('/img/Injectiesysteem Reinigen.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2>Brandstofsysteem Reinigen</h2></div>
         </div>
-        <div class="col-md-3 shadow rounded client ml-2 inner p-0">
-            <img src="{{URL::asset('/img/Koplampen polijsten 3.jpg')}}" class="rounded" alt="profile Pic" height="200" width="367">
+        <div class="col-lg shadow rounded client ml-2 inner p-0">
+            <img src="{{URL::asset('/img/Koplampen polijsten 3.jpg')}}" class="rounded" alt="profile Pic" height="200" width="300">
             <div class="carousel-caption"><h2>Koplampen Polijsten</h2></div>
         </div>
     </div>
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center"> --}}
         {{-- <div class="col-md-3 shadow rounded client inner p-0">Inbouw</div> --}}
         {{-- <div class="col shadow rounded client ml-2 inner p-0"><a href="#about">Aankoopkeuring Auto</a></div> --}}
         {{-- <div class="col-md-3 shadow rounded client inner p-0 ml-2">Airco reinigen</div> --}}
         {{-- <div class="col-md-3 shadow rounded client ml-2 inner p-0">Grote Inspectie</div> --}}
-    </div>
+    {{-- </div> --}}
 @endsection
-
-@section('funny-logo')
-<div class="row funny-animo">
-    <div class="col a-container">
-        <span class="text1">Jij blij!</span><br>
-        <span class="text2">Wij blij!</span>
-    </div>
-</div>
-@endsection
-
 @section('car')
     <div class="row car-container">
         <div class="car-top1">
@@ -188,7 +172,17 @@
             </div>
         </div>
     </div>
-    @endsection
+@endsection
+@section('funny-logo')
+<div class="row funny-animo">
+    <div class="col a-container">
+        <span class="text1">Jij blij!</span><br>
+        <span class="text2">Wij blij!</span>
+    </div>
+</div>
+@endsection
+
+
 
 
    
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,29 +1,27 @@
 @include('/partials/_head')
 
     <body>
-        <div class="container-fluid">
+        <div class="container">
             @yield('head')
         </div>
-        <div class="container-fluid">
+        <div class="container">
             @yield('header')
         </div>
-        <div class="container-fluid jmain">
-            <div class="row">
-                <div class="container-fluid  mx-5">
-                    @yield('gridboven')
-                </div>
-                <div class="container-fluid mx-5">
-                    @yield('content')
-                </div>
-                <div class="container-fluid mx-5">
-                    @yield('gridonder')
-                </div>
+        <div class="container p-0">
+            <div class="container mx-15">
+                @yield('gridboven')
+            </div>
+            <div class="container mx-15">
+                @yield('content')
+            </div>
+            <div class="container mx-15">
+                @yield('gridonder')
             </div>
         </div>
         <div class="container">
             @yield('car')
         </div>
-        <div class="container-fluid">
+        <div class="container">
             @yield('funny-logo')
         </div>
         @include('footer')
